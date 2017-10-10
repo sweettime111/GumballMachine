8f11dfbd8b91abe98a327007292c3b491347ff7f
sweettime111 <30824785+sweettime111@users.noreply.github.com>
2017-10-10 15:40:47 +0700

    add method
    add method String

--- src/GumballMachine.java
+++ src/GumballMachine.java
@@ -84,6 +84,21 @@ public void dispense(){
 		}
 	
 }
+public String toString(){
+	String status = "Gumball Machine";
+	if(state == HAS_QUARTER){
+	    status = status +"\n status : HAS_QUARTER";
+    }else if (state == NO_QUARTER){
+	    status = status +"\n status : NO_QUARTER";
+	}else if (state == SOLD){
+		status = status +"\n status : SOLD";
+	}else if (state == SOLD_OUT){
+		status = status +"\n status : SOLD_OUT";
+	
+	}
+	status = status +"\n count : "+this.count;
+	return status;
+   }
 	
 }
 
