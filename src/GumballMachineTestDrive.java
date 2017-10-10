a6a9dfb4fe626bbe474c966e4a1cb25d2771d421
sweettime111 <30824785+sweettime111@users.noreply.github.com>
2017-10-10 16:32:31 +0700

    add

--- src/GumballMachineTestDrive.java
+++ src/GumballMachineTestDrive.java
@@ -4,8 +4,42 @@ public class GumballMachineTestDrive {
 	public static void main(String[] args) {
 		GumballMachine gumballMachine = new GumballMachine(5);
 		
+		System.out.println(gumballMachine);
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
 		
 		System.out.println(gumballMachine);
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.ejectQuarter();
+		gumballMachine.turnCrank();
+		
+		System.out.println(gumballMachine);
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.toString();
+		gumballMachine.ejectQuarter();
+		
+		System.out.println(gumballMachine);
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		
+		System.out.println(gumballMachine);
+		
+		
+		
+		
+		
+		
 		
 	}
 }
